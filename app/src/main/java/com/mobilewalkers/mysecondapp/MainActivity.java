e9e1e1bd0c8e4066a1f6da7eba5cf6478f36c53e
Maliki Muhammed <malikprog@yahoo.com>
2017-01-27 09:24:56 +0100

    Initial commit

---
+++ app/src/main/java/com/mobilewalkers/mysecondapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mobilewalkers.mysecondapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //TODO complete this task
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
